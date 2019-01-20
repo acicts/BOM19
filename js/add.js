ffe7af11d14762a9b1d86b9cb4b548f7c5212730
STEdirisuriya <supunedirisuriya2002@gmail.com>
2019-01-20 20:18:08 +0530

    add delete function

--- js/add.js
+++ js/add.js
@@ -1,18 +1,18 @@
 var db = firebase.database();
 
 var player = {
-  create: function(data){
+  create: function (data) {
     db.ref('players/').push(data);
   },
-  update: function(){
-    
-  },
-  delete: function(){
+  update: function () {
 
+  },
+  delete: function () {
+    firebase.database().ref('players/' + userId).delete();
   }
 }
 
-function Person(name, school){
+function Person(name, school) {
   this.name = name;
   this.school = school;
   this.marks = 0;
@@ -22,7 +22,7 @@ function Person(name, school){
   this.isFacing = false;
 }
 
-function addOne(){
+function addOne() {
   var n = document.getElementById('player').value;
   var s = document.getElementById('school').value;
   var data = new Person(n, s);
@@ -31,18 +31,21 @@ function addOne(){
   s = "";
 }
 
+function deleteOne() {
+  player.delete(data);
+}
 const playerRef = db.ref('players/');
-const display =   document.getElementById('players');
+const display = document.getElementById('players');
 
 playerRef.on('value', snap => {
   var arr = snap.val();
   var len = Object.keys(arr).length;
   var dp = "  ";
-  for(var i in arr){
+  for (var i in arr) {
     dp += i;
     dp += '<br><ul>';
     var c = arr[i];
-    for(var j in arr[i]){
+    for (var j in arr[i]) {
       dp += "<li>";
       dp += j;
       dp += ":";
@@ -50,8 +53,27 @@ playerRef.on('value', snap => {
       dp += "</li>";
     }
     //dp += JSON.stringify(arr[i]);
+    dp += '<input type="button" onclick="deleteOne()" value="Delete" class="btn btn-primary"></input>'
     dp += '</ul><br>';
   }
-  
+
   display.innerHTML = dp;
+});
+
+const playeridRef = db.ref('players/');
+const displayid = document.getElementById('playerid');
+
+playeridRef.on('value', snap => {
+  var arr = snap.val();
+  var len = Object.keys(arr).length;
+  var dp = "";
+  dp += '<select class="form-control">';
+  for (var i in arr) {
+
+    dp += '<option>';
+    dp += i;
+    dp += '</option>';
+  }
+  dp += '</select>';
+  displayid.innerHTML = dp;
 });
